81253fcd72389ea38ef3982eab8fbeb73cf0deac
MATSUU Takuto <matsuu@gmail.com>
2022-06-21 23:50:13 +0900

    add limit and multiple files

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"text/template"
 	"time"
@@ -37,6 +38,60 @@ var reStmtName = regexp.MustCompile(`^\*ast\.(.*)Stmt$`)
 type SummaryQuery struct {
 	Stmt   *string
 	Tables []string
+}
+
+type OptLimit struct {
+	count   *int
+	percent *float64
+}
+
+var optLimit OptLimit
+
+func (o *OptLimit) Set(v string) error {
+	if strings.HasSuffix(v, "%") {
+		p := strings.TrimSuffix(v, "%")
+		f, err := strconv.ParseFloat(p, 10)
+		if err != nil {
+			return err
+		}
+		if f < 0.0 || 100.0 < f {
+			return fmt.Errorf("Percentage should be in 0-100: %f%%", f)
+		}
+		o.percent = &f
+	} else {
+		u, err := strconv.Atoi(v)
+		if err != nil {
+			return err
+		}
+		if u < 0 {
+			return fmt.Errorf("Limit should be natural number: %d", u)
+		}
+		o.count = &u
+	}
+	return nil
+}
+
+func (o OptLimit) String() string {
+	if o.count != nil {
+		return strconv.Itoa(*o.count)
+	}
+	if o.percent != nil {
+		return strconv.FormatFloat(*o.percent, 'f', -1, 64) + "%"
+	}
+	return "20"
+}
+
+func (o OptLimit) Limit(count int) int {
+	if o.count != nil {
+		if count > *o.count {
+			count = *o.count
+		}
+	} else if o.percent != nil {
+		count = int(float64(count) * *o.percent / 100)
+	} else {
+		count = 20
+	}
+	return count
 }
 
 func (s *SummaryQuery) Enter(in ast.Node) (ast.Node, bool) {
@@ -95,15 +150,23 @@ func anyToFloat64(v any) float64 {
 	return f
 }
 
-func aggregateSlowLog(w io.Writer, r io.ReadSeeker, examples bool, utcOffset time.Duration, outlierTime float64) error {
+func aggregateSlowLog(w io.Writer, rs []io.ReadSeeker, examples bool, utcOffset time.Duration, outlierTime float64) error {
 	opt := log.Options{}
-	p := slow.NewSlowLogParser(r, opt)
-	go p.Start()
 	a := event.NewAggregator(examples, utcOffset, outlierTime)
-	for e := range p.EventChan() {
-		f := query.Fingerprint(e.Query)
-		id := query.Id(f)
-		a.AddEvent(e, id, f)
+
+	var filenames []string
+
+	for _, r := range rs {
+		if file, ok := r.(*os.File); ok {
+			filenames = append(filenames, file.Name())
+		}
+		p := slow.NewSlowLogParser(r, opt)
+		go p.Start()
+		for e := range p.EventChan() {
+			f := query.Fingerprint(e.Query)
+			id := query.Id(f)
+			a.AddEvent(e, id, f)
+		}
 	}
 
 	finalizedResult := a.Finalize()
@@ -112,10 +175,7 @@ func aggregateSlowLog(w io.Writer, r io.ReadSeeker, examples bool, utcOffset tim
 	if err != nil {
 		return err
 	}
-	filename := ""
-	if file, ok := r.(*os.File); ok {
-		filename = file.Name()
-	}
+	filename := strings.Join(filenames, " ")
 	profiles := make([]*event.Class, 0, len(finalizedResult.Class))
 
 	for _, v := range finalizedResult.Class {
@@ -126,12 +186,13 @@ func aggregateSlowLog(w io.Writer, r io.ReadSeeker, examples bool, utcOffset tim
 		return profiles[i].Metrics.TimeMetrics["Query_time"].Sum > profiles[j].Metrics.TimeMetrics["Query_time"].Sum
 	})
 
+	max := optLimit.Limit(len(profiles))
 	result := Result{
 		CurrentDate: &now,
 		Hostname:    hostname,
 		Filename:    filename,
 		Global:      finalizedResult.Global,
-		Profiles:    profiles,
+		Profiles:    profiles[:max],
 	}
 
 	funcMap := template.FuncMap{
@@ -261,19 +322,27 @@ func aggregateSlowLog(w io.Writer, r io.ReadSeeker, examples bool, utcOffset tim
 }
 
 func main() {
-	flag.Parsed()
-	var reader io.ReadSeeker
-	if len(flag.Args()) > 0 {
-		filename := flag.Arg(0)
-		f, err := os.Open(filename)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] [files...]\n", os.Args[0])
+		fmt.Fprintln(flag.CommandLine.Output(), "Options:")
+		flag.PrintDefaults()
+	}
+	flag.Var(&optLimit, "limit", "Limit output to the given percentage or count(default 20)")
+	flag.Parse()
+	var readers []io.ReadSeeker
+	if flag.NArg() > 0 {
+		for _, a := range flag.Args() {
+			f, err := os.Open(a)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			defer f.Close()
+			readers = append(readers, f)
 		}
-		defer f.Close()
-		reader = f
 	} else {
 		fmt.Fprintln(os.Stderr, "Reading from STDIN ...")
-		reader = os.Stdin
+		readers = append(readers, os.Stdin)
 	}
 	_, offset := time.Now().Zone()
 	utcOffset := time.Duration(offset) * time.Second
@@ -282,7 +351,7 @@ func main() {
 
 	writer := os.Stdout
 
-	err := aggregateSlowLog(writer, reader, true, utcOffset, outlierTime)
+	err := aggregateSlowLog(writer, readers, true, utcOffset, outlierTime)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
