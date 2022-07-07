c8210c3a80a534b83cc4ba2aefce143fd79d8c28
MATSUU Takuto <matsuu@gmail.com>
2022-07-07 22:47:15 +0900

    refactoring

--- go.mod
+++ go.mod
@@ -4,20 +4,17 @@ go 1.18
 
 require (
 	github.com/percona/go-mysql v0.0.0-20210708085315-3e7f9d34c354
-	github.com/pingcap/parser v0.0.0-20200623164729-3a18f1e5dceb
+	github.com/pingcap/tidb/parser v0.0.0-20211124132551-4a1b2e9fe5b5
 )
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/pingcap/errors v0.11.4 // indirect
-	github.com/pingcap/log v0.0.0-20191012051959-b742a5d432e9 // indirect
-	go.uber.org/atomic v1.5.0 // indirect
-	go.uber.org/multierr v1.3.0 // indirect
-	go.uber.org/tools v0.0.0-20190618225709-2cfd321de3ee // indirect
-	go.uber.org/zap v1.12.0 // indirect
-	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
-	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
-	golang.org/x/tools v0.0.0-20191029190741-b9c20aec41a5 // indirect
+	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548 // indirect
+	github.com/pingcap/errors v0.11.5-0.20210425183316-da1aaba5fb63 // indirect
+	github.com/pingcap/log v0.0.0-20210625125904-98ed8e2eb1c7 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.18.1 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
-	honnef.co/go/tools v0.0.1-2019.2.3 // indirect
 )
--- main.go
+++ main.go
@@ -18,287 +18,52 @@ import (
 	"github.com/percona/go-mysql/log"
 	"github.com/percona/go-mysql/log/slow"
 	"github.com/percona/go-mysql/query"
-	"github.com/pingcap/parser"
-	"github.com/pingcap/parser/ast"
-	_ "github.com/pingcap/parser/test_driver"
+	"github.com/pingcap/tidb/parser"
+	"github.com/pingcap/tidb/parser/ast"
+	_ "github.com/pingcap/tidb/parser/test_driver"
 )
 
-type Result struct {
-	CurrentDate *time.Time
-	Hostname    string
-	Filename    string
-	Global      *event.Class
-	Profiles    []*event.Class
-	Width       Width
-}
-
-func (r Result) widthRank() int {
-	w := getIntWidth(float64(r.Global.UniqueQueries))
-	if w < 4 {
-		w = 4
+func NewAnalyzer(examples bool, utcOffset time.Duration, outlierTime float64, limit *OptLimit) *Analyzer {
+	ag := event.NewAggregator(examples, utcOffset, outlierTime)
+	return &Analyzer{
+		aggregator: ag,
+		limit:      limit,
 	}
-	return w
 }
 
-func (r Result) DrawRankHeader() string {
-	w := r.widthRank()
-	s := "Rank"
-	return s + strings.Repeat(" ", w-len(s))
+type Analyzer struct {
+	opt        log.Options
+	filenames  []string
+	aggregator *event.Aggregator
+	limit      *OptLimit
 }
 
-func (r Result) DrawRankHR() string {
-	w := r.widthRank()
-	return strings.Repeat("=", w)
-}
-
-func (r Result) FormatRank(v int) string {
-	w := r.widthRank()
-	f := fmt.Sprintf("%%%dd", w)
-	return fmt.Sprintf(f, v+1)
-}
-
-func (r Result) widthResponseTime() int {
-	w := getIntWidth(*r.Global.Metrics.TimeMetrics["Query_time"].Max)
-	if w < 2 {
-		w = 2
+func (an *Analyzer) Analyze(r io.ReadSeeker) error {
+	if file, ok := r.(*os.File); ok {
+		an.filenames = append(an.filenames, file.Name())
 	}
-	// Int.xxxx
-	w += 5
-	return w
-}
+	p := slow.NewSlowLogParser(r, an.opt)
 
-func (r Result) DrawResponseTimeHeader() string {
-	w := r.widthResponseTime() + 7
-	s := "Response time"
-	return s + strings.Repeat(" ", w-len(s))
-}
-
-func (r Result) DrawResponseTimeHR() string {
-	w := r.widthResponseTime() + 7
-	return strings.Repeat("=", w)
-}
-
-func (r Result) FormatResponseTime(v float64) string {
-	w := r.widthResponseTime()
-	f := fmt.Sprintf("%%%d.4f", w)
-	return fmt.Sprintf(f, v)
-}
-
-func (r Result) widthCalls() int {
-	w := getIntWidth(float64(r.Global.TotalQueries))
-	if w < 5 {
-		w = 5
-	}
-	return w
-}
-
-func (r Result) FormatCalls(v uint) string {
-	w := r.widthCalls()
-	f := fmt.Sprintf("%%%dd", w)
-	return fmt.Sprintf(f, v)
-}
-
-func (r Result) DrawCallsHeader() string {
-	w := r.widthCalls()
-	s := "Calls"
-	return s + strings.Repeat(" ", w-len(s))
-}
-func (r Result) DrawCallsHR() string {
-	w := r.widthCalls()
-	return strings.Repeat("=", w)
-}
-
-func (r Result) widthRPCall() int {
-	return r.Width.RPCall
-}
-
-func (r Result) DrawRPCallHR() string {
-	w := r.widthRPCall()
-	return strings.Repeat("=", w)
-}
-
-func (r Result) FormatRPCall() string {
-	w := r.widthRPCall()
-	return fmt.Sprintf("%%%ds", w)
-}
-
-//go:embed templates
-var fs embed.FS
-
-var reStmtName = regexp.MustCompile(`^\*ast\.(.*)Stmt$`)
-
-type SummaryQuery struct {
-	Stmt   *string
-	Tables []string
-}
-
-type OptLimit struct {
-	count   *int
-	percent *float64
-}
-
-var optLimit OptLimit
-
-type Width struct {
-	TotalQueries int
-	RPCall       int
-}
-
-func getIntWidth(v float64) int {
-	w := 1
-
-	abs := math.Abs(v)
-	if abs != v {
-		w++
-	}
-	if abs >= 1.0 {
-		w += int(math.Log10(math.Abs(v)))
-	}
-	return w
-}
-
-func (w *Width) Calc(c *event.Class) error {
-	t := getIntWidth(float64(c.TotalQueries))
-	if w.TotalQueries < t {
-		w.TotalQueries = t
-	}
-
-	r := getIntWidth(c.Metrics.TimeMetrics["Query_time"].Sum/float64(c.TotalQueries)) + 5
-	if w.RPCall < r {
-		w.RPCall = r
+	go p.Start()
+	for e := range p.EventChan() {
+		f := query.Fingerprint(e.Query)
+		id := query.Id(f)
+		an.aggregator.AddEvent(e, id, f)
 	}
 	return nil
 }
 
-func (o *OptLimit) Set(v string) error {
-	if strings.HasSuffix(v, "%") {
-		p := strings.TrimSuffix(v, "%")
-		f, err := strconv.ParseFloat(p, 10)
-		if err != nil {
-			return err
-		}
-		if f < 0.0 || 100.0 < f {
-			return fmt.Errorf("Percentage should be in 0-100: %f%%", f)
-		}
-		o.percent = &f
-	} else {
-		u, err := strconv.Atoi(v)
-		if err != nil {
-			return err
-		}
-		if u < 0 {
-			return fmt.Errorf("Limit should be natural number: %d", u)
-		}
-		o.count = &u
-	}
-	return nil
-}
+func (an *Analyzer) Finalize(w io.Writer) error {
 
-func (o OptLimit) String() string {
-	if o.count != nil {
-		return strconv.Itoa(*o.count)
-	}
-	if o.percent != nil {
-		return strconv.FormatFloat(*o.percent, 'f', -1, 64) + "%"
-	}
-	return "20"
-}
+	finalizedResult := an.aggregator.Finalize()
 
-func (o OptLimit) Limit(count int) int {
-	if o.count != nil {
-		if count > *o.count {
-			count = *o.count
-		}
-	} else if o.percent != nil {
-		count = int(float64(count) * *o.percent / 100)
-	} else {
-		count = 20
-	}
-	return count
-}
-
-func (s *SummaryQuery) Enter(in ast.Node) (ast.Node, bool) {
-	if s.Stmt == nil {
-		t := fmt.Sprintf("%T", in)
-		str := reStmtName.ReplaceAllString(t, "$1")
-		var b strings.Builder
-		for i, s := range str {
-			if i != 0 && s < 'a' {
-				b.WriteRune(' ')
-			}
-			b.WriteRune(s)
-		}
-		stmt := strings.ToUpper(b.String())
-		s.Stmt = &stmt
-	}
-	if table, ok := in.(*ast.TableName); ok {
-		var names []string
-		if table.Schema.O != "" {
-			names = append(names, table.Schema.O)
-		}
-		names = append(names, table.Name.O)
-		s.Tables = append(s.Tables, strings.Join(names, "."))
-	}
-	return in, false
-}
-
-func (s *SummaryQuery) Leave(in ast.Node) (ast.Node, bool) {
-	return in, true
-}
-
-func (s *SummaryQuery) String() string {
-	var stmt string
-	if s.Stmt != nil {
-		stmt = *s.Stmt
-	}
-	return fmt.Sprintf("%s %s", stmt, strings.Join(s.Tables, " "))
-}
-
-func anyToFloat64(v any) float64 {
-	var f float64
-	switch v.(type) {
-	case uint:
-		f = float64(v.(uint))
-	case uint64:
-		f = float64(v.(uint64))
-	case *uint64:
-		f = float64(*v.(*uint64))
-	case float64:
-		f = float64(v.(float64))
-	case *float64:
-		f = float64(*v.(*float64))
-	default:
-		fmt.Fprintf(os.Stderr, "unknown type: %T", v)
-	}
-	return f
-}
-
-func aggregateSlowLog(w io.Writer, rs []io.ReadSeeker, examples bool, utcOffset time.Duration, outlierTime float64) error {
-	opt := log.Options{}
-	a := event.NewAggregator(examples, utcOffset, outlierTime)
-
-	var filenames []string
-
-	for _, r := range rs {
-		if file, ok := r.(*os.File); ok {
-			filenames = append(filenames, file.Name())
-		}
-		p := slow.NewSlowLogParser(r, opt)
-		go p.Start()
-		for e := range p.EventChan() {
-			f := query.Fingerprint(e.Query)
-			id := query.Id(f)
-			a.AddEvent(e, id, f)
-		}
-	}
-
-	finalizedResult := a.Finalize()
 	now := time.Now()
 	hostname, err := os.Hostname()
+
 	if err != nil {
 		return err
 	}
-	filename := strings.Join(filenames, " ")
+	filename := strings.Join(an.filenames, " ")
 	profiles := make([]*event.Class, 0, len(finalizedResult.Class))
 
 	var width Width
@@ -314,7 +79,7 @@ func aggregateSlowLog(w io.Writer, rs []io.ReadSeeker, examples bool, utcOffset 
 		return profiles[i].Metrics.TimeMetrics["Query_time"].Sum > profiles[j].Metrics.TimeMetrics["Query_time"].Sum
 	})
 
-	max := optLimit.Limit(len(profiles))
+	max := an.limit.Limit(len(profiles))
 	result := Result{
 		CurrentDate: &now,
 		Hostname:    hostname,
@@ -450,12 +215,262 @@ func aggregateSlowLog(w io.Writer, rs []io.ReadSeeker, examples bool, utcOffset 
 	return tmpl.ExecuteTemplate(w, "report.tmpl", result)
 }
 
+type Result struct {
+	CurrentDate *time.Time
+	Hostname    string
+	Filename    string
+	Global      *event.Class
+	Profiles    []*event.Class
+	Width       Width
+}
+
+func (r Result) widthRank() int {
+	w := getIntWidth(float64(r.Global.UniqueQueries))
+	if w < 4 {
+		w = 4
+	}
+	return w
+}
+
+func (r Result) DrawRankHeader() string {
+	w := r.widthRank()
+	s := "Rank"
+	return s + strings.Repeat(" ", w-len(s))
+}
+
+func (r Result) DrawRankHR() string {
+	w := r.widthRank()
+	return strings.Repeat("=", w)
+}
+
+func (r Result) FormatRank(v int) string {
+	w := r.widthRank()
+	f := fmt.Sprintf("%%%dd", w)
+	return fmt.Sprintf(f, v+1)
+}
+
+func (r Result) widthResponseTime() int {
+	w := getIntWidth(*r.Global.Metrics.TimeMetrics["Query_time"].Max)
+	if w < 2 {
+		w = 2
+	}
+	// Int.xxxx
+	w += 5
+	return w
+}
+
+func (r Result) DrawResponseTimeHeader() string {
+	w := r.widthResponseTime() + 7
+	s := "Response time"
+	return s + strings.Repeat(" ", w-len(s))
+}
+
+func (r Result) DrawResponseTimeHR() string {
+	w := r.widthResponseTime() + 7
+	return strings.Repeat("=", w)
+}
+
+func (r Result) FormatResponseTime(v float64) string {
+	w := r.widthResponseTime()
+	f := fmt.Sprintf("%%%d.4f", w)
+	return fmt.Sprintf(f, v)
+}
+
+func (r Result) widthCalls() int {
+	w := getIntWidth(float64(r.Global.TotalQueries))
+	if w < 5 {
+		w = 5
+	}
+	return w
+}
+
+func (r Result) FormatCalls(v uint) string {
+	w := r.widthCalls()
+	f := fmt.Sprintf("%%%dd", w)
+	return fmt.Sprintf(f, v)
+}
+
+func (r Result) DrawCallsHeader() string {
+	w := r.widthCalls()
+	s := "Calls"
+	return s + strings.Repeat(" ", w-len(s))
+}
+func (r Result) DrawCallsHR() string {
+	w := r.widthCalls()
+	return strings.Repeat("=", w)
+}
+
+func (r Result) widthRPCall() int {
+	return r.Width.RPCall
+}
+
+func (r Result) DrawRPCallHR() string {
+	w := r.widthRPCall()
+	return strings.Repeat("=", w)
+}
+
+func (r Result) FormatRPCall() string {
+	w := r.widthRPCall()
+	return fmt.Sprintf("%%%ds", w)
+}
+
+//go:embed templates
+var fs embed.FS
+
+var reStmtName = regexp.MustCompile(`^\*ast\.(.*)Stmt$`)
+
+type SummaryQuery struct {
+	Stmt   *string
+	Tables []string
+}
+
+type OptLimit struct {
+	count   *int
+	percent *float64
+}
+
+type Width struct {
+	TotalQueries int
+	RPCall       int
+}
+
+func getIntWidth(v float64) int {
+	w := 1
+
+	abs := math.Abs(v)
+	if abs != v {
+		w++
+	}
+	if abs >= 1.0 {
+		w += int(math.Log10(math.Abs(v)))
+	}
+	return w
+}
+
+func (w *Width) Calc(c *event.Class) error {
+	t := getIntWidth(float64(c.TotalQueries))
+	if w.TotalQueries < t {
+		w.TotalQueries = t
+	}
+
+	r := getIntWidth(c.Metrics.TimeMetrics["Query_time"].Sum/float64(c.TotalQueries)) + 5
+	if w.RPCall < r {
+		w.RPCall = r
+	}
+	return nil
+}
+
+func (o *OptLimit) Set(v string) error {
+	if strings.HasSuffix(v, "%") {
+		p := strings.TrimSuffix(v, "%")
+		f, err := strconv.ParseFloat(p, 10)
+		if err != nil {
+			return err
+		}
+		if f < 0.0 || 100.0 < f {
+			return fmt.Errorf("Percentage should be in 0-100: %f%%", f)
+		}
+		o.percent = &f
+	} else {
+		u, err := strconv.Atoi(v)
+		if err != nil {
+			return err
+		}
+		if u < 0 {
+			return fmt.Errorf("Limit should be natural number: %d", u)
+		}
+		o.count = &u
+	}
+	return nil
+}
+
+func (o OptLimit) String() string {
+	if o.count != nil {
+		return strconv.Itoa(*o.count)
+	}
+	if o.percent != nil {
+		return strconv.FormatFloat(*o.percent, 'f', -1, 64) + "%"
+	}
+	return "20"
+}
+
+func (o OptLimit) Limit(count int) int {
+	if o.count != nil {
+		if count > *o.count {
+			count = *o.count
+		}
+	} else if o.percent != nil {
+		count = int(float64(count) * *o.percent / 100)
+	} else {
+		count = 20
+	}
+	return count
+}
+
+func (s *SummaryQuery) Enter(in ast.Node) (ast.Node, bool) {
+	if s.Stmt == nil {
+		t := fmt.Sprintf("%T", in)
+		str := reStmtName.ReplaceAllString(t, "$1")
+		var b strings.Builder
+		for i, s := range str {
+			if i != 0 && s < 'a' {
+				b.WriteRune(' ')
+			}
+			b.WriteRune(s)
+		}
+		stmt := strings.ToUpper(b.String())
+		s.Stmt = &stmt
+	}
+	if table, ok := in.(*ast.TableName); ok {
+		var names []string
+		if table.Schema.O != "" {
+			names = append(names, table.Schema.O)
+		}
+		names = append(names, table.Name.O)
+		s.Tables = append(s.Tables, strings.Join(names, "."))
+	}
+	return in, false
+}
+
+func (s *SummaryQuery) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
+
+func (s *SummaryQuery) String() string {
+	var stmt string
+	if s.Stmt != nil {
+		stmt = *s.Stmt
+	}
+	return fmt.Sprintf("%s %s", stmt, strings.Join(s.Tables, " "))
+}
+
+func anyToFloat64(v any) float64 {
+	var f float64
+	switch v.(type) {
+	case uint:
+		f = float64(v.(uint))
+	case uint64:
+		f = float64(v.(uint64))
+	case *uint64:
+		f = float64(*v.(*uint64))
+	case float64:
+		f = float64(v.(float64))
+	case *float64:
+		f = float64(*v.(*float64))
+	default:
+		fmt.Fprintf(os.Stderr, "unknown type: %T", v)
+	}
+	return f
+}
+
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] [files...]\n", os.Args[0])
 		fmt.Fprintln(flag.CommandLine.Output(), "Options:")
 		flag.PrintDefaults()
 	}
+
+	var optLimit OptLimit
 	flag.Var(&optLimit, "limit", "Limit output to the given percentage or count(default 20)")
 	flag.Parse()
 	var readers []io.ReadSeeker
@@ -480,8 +495,16 @@ func main() {
 
 	writer := os.Stdout
 
-	err := aggregateSlowLog(writer, readers, true, utcOffset, outlierTime)
-	if err != nil {
+	a := NewAnalyzer(true, utcOffset, outlierTime, &optLimit)
+	for _, r := range readers {
+		err := a.Analyze(r)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+	if err := a.Finalize(writer); err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
